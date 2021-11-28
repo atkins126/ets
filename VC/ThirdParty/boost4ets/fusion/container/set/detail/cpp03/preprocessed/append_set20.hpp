dce79ad519cd273317e5d0dde79c7b2ad856df7d
Steven <xinghun87@163.com>
2021-11-28 14:10:44 +0800

    添加版权声明

--- VC/ThirdParty/boost4ets/fusion/container/set/detail/cpp03/preprocessed/append_set20.hpp
+++ VC/ThirdParty/boost4ets/fusion/container/set/detail/cpp03/preprocessed/append_set20.hpp
@@ -1,11 +1,17 @@
-/*=============================================================================
-    Copyright (c) 2001-2011 Joel de Guzman
+/*
+    Copyright (c) 2021-2031 Steven Shi
 
-    Distributed under the Boost Software License, Version 1.0. (See accompanying
-    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+    ETS(Extended Tool Set)，可扩展工具集。
 
-    This is an auto-generated file. Do not edit!
-==============================================================================*/
+    本工具软件是开源自由软件，您可以遵照 MIT 协议，修改和发布此程序。
+    发布此库的目的是希望其有用，但不做任何保证。
+    如果将本库用于商业项目，由于本库中的Bug，而引起的任何风险及损失，本作者不承担任何责任。
+
+    开源地址: https://github.com/ets-ddui/ets
+    开源协议: The MIT License (MIT)
+    作者邮箱: xinghun87@163.com
+    官方博客：https://blog.csdn.net/xinghun61
+*/
 namespace ets { namespace fusion { namespace detail
 {
     template <>
